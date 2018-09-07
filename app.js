2c7a55ff9e807a02715f575223d8b45f9693d85b
Ying Wang <ext-ying.wang@here.com>
2018-09-08 00:56:10 +0200

    setup the backend server

--- app.js
+++ app.js
@@ -9,11 +9,30 @@ var io = require('socket.io')(http);
 
 let port = process.env.PORT || 10050;
 
+let numberOfPlayers = 0;
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+
+  numberOfPlayers += 1;
+  console.log(`User ${socket.id} connected`);
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    numberOfPlayers -= 1;
+    console.log(`User ${socket.id} disconnected`);
   });
+  if (numberOfPlayers === 2) {
+    socket.emit('start');
+  }
+
+  socket.on('number', function(number) {
+    console.log(`received ${number} from ${socket.id}`);
+    if (number === 1) {
+      socket.emit('game-over', { win: true });
+      socket.broadcast.emit('game-over', { win: false });
+    } else {
+      socket.broadcast.emit('number', number);
+    }
+  });
+
 });
 
 http.listen(port, function(){
